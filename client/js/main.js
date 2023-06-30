6fa1f2e64f4c5d80b9f930ab34c169eb80141f12
Jeremie Bents <jeremie.bents@sap.com>
2023-06-30 14:13:33 +0200

    Button functionality "delete", "enter"

--- client/js/main.js
+++ client/js/main.js
@@ -13,10 +13,6 @@ if(document.cookie.match(/theme=dark/) != null) {
 
 document.addEventListener("DOMContentLoaded", initBoard);
 
-document.addEventListener("DOMContentLoaded", initBoard);
-
-let newGame  = document.getElementById('newGame');
-newGame.addEventListener('click', restart, true);
 // start the game and sets a new word in the backend();
 const start = () => {
   fetch(`http://localhost:8000/api/game/`,{
