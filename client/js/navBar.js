21febbab788c5b6448212c905e9641b2d787b85d
Jeremie Bents´ <Jeremie23104@gmail.com>
2023-07-04 18:46:24 +0200

    adjust navbar 1.0

--- client/js/navBar.js
+++ client/js/navBar.js
@@ -7,33 +7,40 @@ fetch('../html/navbar.html')
       document.body.insertBefore(navbarContainer, document.body.firstChild);
 });
 
-
-const homeTab = document.getElementById("Home");
-homeTab.addEventListener("click", ()=>{
-    homeTab.classList.add("active");
-    leaderBoardTab.classList.remove("active)");
-    settingsTab.classList.remove("active)");
-    contactUsTab.classList.remove("active)");
-})
-const leaderBoardTab = document.getElementById("leaderboard");
-leaderBoardTab.addEventListener("click", ()=>{
-    homeTab.classList.remove("active");
-    leaderBoardTab.classList.add("active)");
-    settingsTab.classList.remove("active)");
-    contactUsTab.classList.remove("active)");
-})
-const settingsTab = document.getElementById("settings");
-settingsTab.addEventListener("click", ()=>{
-    homeTab.classList.remove("active");
-    leaderBoardTab.classList.remove("active)");
-    settingsTab.classList.add("active)");
-    contactUsTab.classList.remove("active)");
-})
-const contactUsTab = document.getElementById("contactUs");
-contactUsTab.addEventListener("click", ()=>{
-    console.log("Hallo Contact Us")
-    homeTab.classList.remove("active");
-    leaderBoardTab.classList.remove("active)");
-    settingsTab.classList.add("active)");
-    contactUsTab.classList.add("active)");
-})
+let homeTab = document.getElementById("Home");
+if(homeTab){
+    homeTab.addEventListener("click", ()=>{
+        homeTab.classList.add("active");
+        leaderBoardTab.classList.remove("active)");
+        settingsTab.classList.remove("active)");
+        contactUsTab.classList.remove("active)");
+    })
+}
+let leaderBoardTab = document.getElementById("leaderboard");
+if(leaderBoardTab){
+    leaderBoardTab.addEventListener("click", ()=>{
+        homeTab.classList.remove("active");
+        leaderBoardTab.classList.add("active)");
+        settingsTab.classList.remove("active)");
+        contactUsTab.classList.remove("active)");
+    })
+}
+let settingsTab = document.getElementById("settings");
+if(settingsTab){
+    settingsTab.addEventListener("click", ()=>{
+        homeTab.classList.remove("active");
+        leaderBoardTab.classList.remove("active)");
+        settingsTab.classList.add("active)");
+        contactUsTab.classList.remove("active)");
+    })
+}
+let contactUsTab = document.getElementById("contactUs");
+if(contactUsTab){
+    contactUsTab.addEventListener("click", ()=>{
+        console.log("Hallo Contact Us")
+        homeTab.classList.remove("active");
+        leaderBoardTab.classList.remove("active)");
+        settingsTab.classList.add("active)");
+        contactUsTab.classList.add("active)");
+    })
+}
