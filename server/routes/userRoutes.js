4ca44eb773635e3a2bad874ddb0956a42a1676a8
Gerrit <gerrit.graetz@gmail.com>
2023-07-07 13:16:52 +0200

    added backend routes for changeUsername and changePassword

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,11 +1,13 @@
 import express from 'express';
 //import User functions
-import { getSecurityQuestion, resetPassword } from "../controllers/userController.js";
+import { getSecurityQuestion, resetPassword, changePassword, changeUsername } from "../controllers/userController.js";
 
 //create Router from express
 const userRouter = express.Router();
 
 userRouter.post("/resetPassword", resetPassword);
 userRouter.post("/getSecurityQuestion", getSecurityQuestion);
+userRouter.post("/changePassword", changePassword);
+userRouter.post("/changeUsername", changeUsername);
 
 export default userRouter;
